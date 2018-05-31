78db199a6e1b7d82ee326e32dd55aee435619753
zhaojintian <729234283@qq.com>
2018-05-31 18:02:06 +0800

    网络请求封装

---
+++ src/utils/consts.js
@@ -0,0 +1,14 @@
+// 接口地址
+const API_URL = 'http://apitest.shenbd.com'
+
+// 签名密钥
+const SIGN_KEY = '~!@#$`1234qwert'
+
+// 设备类型
+const DEVICE_TYPE = 5
+
+export default {
+  API_URL,
+  SIGN_KEY,
+  DEVICE_TYPE
+}
---
+++ src/utils/request.js
@@ -0,0 +1,26 @@
+import wepy from 'wepy'
+import consts from './consts'
+import Hashes from 'jshashes'
+
+export default async ({url = '', method = 'GET', data = {}, requiresAuth = true} = {}) => {
+  const key = consts.SIGN_KEY
+  const deviceType = consts.DEVICE_TYPE
+  const nonce = Math.random().toString(36).substr(7)
+  const timestamp = new Date().getTime()
+
+  data = Object.assign({deviceType, nonce, timestamp}, data)
+
+  const text = Object.keys(data).sort((key1, key2) => key1.charAt(0) > key2.charAt(0) ? 1 : -1).map(key => data[key]).join('')
+  const sign = new Hashes.MD5().hex(text + key)
+
+  return wepy.request({
+    url: consts.API_URL + url,
+    method,
+    header: {
+      'content-type': 'application/x-www-form-urlencoded;'
+    },
+    data: Object.keys(data).map(key => {
+      return `data[${key}]=${data[key]}`
+    }).join('&') + `&sign=${sign}`
+  })
+}
