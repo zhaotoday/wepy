36c58586a42f9d3721db9b2d85a3cf461b997e06
zhaojintian <729234283@qq.com>
2018-06-02 23:00:05 +0800

    优化

---
+++ src/mock/memberCenter.index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  'status': 'success',
+  'resultCode': 1,
+  'longMessage': '',
+  'shortMessage': '',
+  'encryptType': 3,
+  'requestId': '1804276455942XDGGRF',
+  'responseTime': 10,
+  'requestTime': 10,
+  'data': {
+    'key': '26c50ebac55b8a6db60dba8f641b5362'
+  },
+  'responseContent': {
+    'memberName': 'zzz',
+    'memberAvatar': 'http://qm.22ca.com/data/upload/1.jpg',
+    'goodsCollection': 25,
+    'storeCollection': 26,
+    'footprint': 112,
+    'waitPay': 1,
+    'waitShipments': 1,
+    'waitReceiving': 2,
+    'waitEvaluate': 2,
+    'refund': 0,
+    'isDeliveryman': 0,
+    'isSeller': 1,
+    'isSalesman': 0
+  }
+}
