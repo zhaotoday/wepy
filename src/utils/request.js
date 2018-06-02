9461662f7f41b9af4b008daad52634b4a03c4404
zhaojintian <729234283@qq.com>
2018-06-02 22:57:54 +0800

    优化

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,30 +7,50 @@ export default async (
     url = '',
     method = 'GET',
     data = {},
-    requiresAuth = true
+    requiresAuth = true,
+    mock = false
   } = {}
 ) => {
+  const header = {
+    'content-type': 'application/x-www-form-urlencoded;'
+  }
   const key = consts.SIGN_KEY
-  const deviceType = consts.DEVICE_TYPE
-  const nonce = Math.random().toString(36).substr(7)
-  const timestamp = new Date().getTime()
-
-  data = Object.assign({deviceType, nonce, timestamp}, data)
-
   const text = Object.keys(data)
     .sort((key1, key2) => key1.charAt(0) > key2.charAt(0) ? 1 : -1)
     .map(key => data[key])
     .join('')
   const sign = new Hashes.MD5().hex(text + key)
 
-  return wepy.request({
-    url: consts.API_URL + url,
-    method,
-    header: {
-      'content-type': 'application/x-www-form-urlencoded;'
-    },
-    data: Object.keys(data)
-      .map(key => `data[${key}]=${data[key]}`)
-      .join('&') + `&sign=${sign}`
-  })
+  // mock 数据
+  if (mock) {
+    const fileName = url.replace(/\//g, '.')
+    const mockData = require(`../mock/${fileName}`)
+
+    console.log(`mock request - url: ${url}, method: ${method}, data: ${JSON.stringify(data)}`)
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(mockData.responseContent)
+      }, 200)
+    })
+  } else {
+    const requestRes = await wepy.request({
+      url: `${consts.API_URL}/${url}`,
+      method,
+      header,
+      data: Object.keys(data)
+        .map(key => `data[${key}]=${data[key]}`)
+        .join('&') + `&sign=${sign}`
+    })
+
+    const {status, responseContent} = requestRes
+
+    return new Promise((resolve, reject) => {
+      if (status === 'success') {
+        resolve(responseContent)
+      } else {
+        reject({status})
+      }
+    })
+  }
 }
