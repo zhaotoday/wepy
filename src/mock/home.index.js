68be7cf155af836009e0b0980ca3f073760790a8
zhaojintian <729234283@qq.com>
2018-06-09 23:06:35 +0800

    mock

---
+++ src/mock/home.index.js
@@ -0,0 +1,169 @@
+module.exports = {
+  'responseContent': {
+    'currentAddress': {
+      'returnAddressId': 108,
+      'choosedAddressFlag': 1,
+      'addresses': [
+        {
+          'addressId': 108,
+          'address': '福远文创园',
+          'tagType': 1
+        }
+      ]
+    },
+    'advs': {
+      'popUpAdv': {
+        'href': 'http://qm.22ca.com/qm/tmpl/goods.html?goods_commonid=101654',
+        'imageUrl': 'http://qm.22ca.com/data/upload/mall/adv/05715959543890495.jpg'
+      },
+      'topAdvs': [
+        {
+          'href': 'http://qm.22ca.com/qm/tmpl/goods.html?goods_commonid=101654',
+          'imageUrl': 'http://qm.22ca.com/data/upload/mall/adv/05715959543890495.jpg'
+        },
+        {
+          'href': '',
+          'imageUrl': 'http://qm.22ca.com/data/upload/mall/adv/05730599933071664.png'
+        },
+        {
+          'href': '',
+          'imageUrl': 'http://qm.22ca.com/data/upload/mall/adv/05730600094085575.png'
+        }
+      ],
+      'middleAdvs': [
+        {
+          'href': 'http://qm.22ca.com/qm/tmpl/goods.html?goods_commonid=101654',
+          'imageUrl': 'http://qm.22ca.com/data/upload/mall/adv/05715959543890495.jpg'
+        },
+        {
+          'href': '',
+          'imageUrl': 'http://qm.22ca.com/data/upload/mall/adv/05730599933071664.png'
+        },
+        {
+          'href': '',
+          'imageUrl': 'http://qm.22ca.com/data/upload/mall/adv/05730600094085575.png'
+        }
+      ]
+    },
+    'recommendClasses': {
+      'totalCount': 8,
+      'classes': [
+        {
+          'gcName': '家居生活',
+          'gcId': '1036',
+          'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1036.jpg'
+        },
+        {
+          'gcName': '服装鞋帽',
+          'gcId': '1037',
+          'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1037.jpg'
+        },
+        {
+          'gcName': '母婴',
+          'gcId': '1058',
+          'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1058.jpg'
+        },
+        {
+          'gcName': '美食',
+          'gcId': '1073',
+          'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1073.jpg'
+        },
+        {
+          'gcName': '超市',
+          'gcId': '1091',
+          'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1091.jpg'
+        },
+        {
+          'gcName': '图书',
+          'gcId': '1064',
+          'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1064.jpg'
+        },
+        {
+          'gcName': '家用电器',
+          'gcId': '1074',
+          'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1074.jpg'
+        },
+        {
+          'gcName': '花',
+          'gcId': '1137',
+          'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1137.jpg'
+        }
+      ]
+    },
+    'nearbyStores': {
+      'totalCount': 4,
+      'stores': [
+        {
+          'storeId': '11',
+          'storeLabel': 'http://qm.22ca.com/data/upload/mall/store/05738418723171995.jpeg',
+          'storeName': 'kkk测试店',
+          'storeIntro': '订单满38起送，满88免服务费',
+          'storeDistance': 3.89
+        },
+        {
+          'storeId': '44',
+          'storeLabel': 'http://qm.22ca.com/data/upload/mall/store/05742631234841410.png',
+          'storeName': 'lmd的店',
+          'storeIntro': '测试公告内容测试公告内容测试公告内容测试公告内容测试公告内容',
+          'storeDistance': 12.47
+        },
+        {
+          'storeId': '13',
+          'storeLabel': 'http://qm.22ca.com/data/upload/mall/store/05739205839527865.jpg',
+          'storeName': 'csw2的店铺',
+          'storeIntro': '春运时期运费加倍，请及早下单，免除后顾之忧',
+          'storeDistance': 0.74
+        }
+      ]
+    },
+    'recommendGoods': {
+      'totalCount': 5,
+      'goodsList': [
+        {
+          'goodsCommonId': 102901,
+          'goodsName': 'Midea/美的 M3-L233B微波炉家用 烧烤蒸烤箱一体智能多功能光波炉',
+          'goodsImage': 'http://qm.22ca.com/data/upload/mall/store/goods/11/11_05750457948968267_240.jpg',
+          'evalScore': 4.6
+        },
+        {
+          'goodsCommonId': 102072,
+          'goodsName': '米菲绘本第一辑10册',
+          'goodsImage': 'http://qm.22ca.com/data/upload/mall/store/goods/8/8_05715037425562382_240.jpg',
+          'evalScore': 4.9
+        },
+        {
+          'goodsCommonId': 102073,
+          'goodsName': '有你真好（全三册）',
+          'goodsImage': 'http://qm.22ca.com/data/upload/mall/store/goods/8/8_05715036381340558_240.jpg',
+          'evalScore': 4.5
+        },
+        {
+          'goodsCommonId': 102074,
+          'goodsName': '文集：恋情的终结',
+          'goodsImage': 'http://qm.22ca.com/data/upload/mall/store/goods/8/8_05715033927196300_240.jpg',
+          'evalScore': 4.9
+        },
+        {
+          'goodsCommonId': 102075,
+          'goodsName': '雪落香杉树"',
+          'goodsImage': 'http://qm.22ca.com/data/upload/mall/store/goods/8/8_05715029553431750_240.jpg',
+          'evalScore': 5.0
+        }
+      ]
+    }
+  },
+  'status': 'success',
+  'resultCode': 1,
+  'longMessage': '',
+  'shortMessage': '操作成功',
+  'encryptType': '3',
+  'requestId': '',
+  'responseTime': 10,
+  'requestTime': 10,
+  'data': {
+    'key': '26c50ebac55b8a6db60dba8f641b5362',
+    'lng': 119.262117,
+    'lat': 26.051754,
+    'cityCode': '0591'
+  }
+}
