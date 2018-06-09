06df05a128ca1f55a3bbc48d54a98da4e6909258
zhaojintian <729234283@qq.com>
2018-06-10 01:00:25 +0800

    优化

---
+++ src/mock/home.getAllClasses.js
@@ -0,0 +1,74 @@
+module.exports = {
+  'status': 'success',
+  'resultCode': 1,
+  'longMessage': '',
+  'shortMessage': '操作成功',
+  'encryptType': '3',
+  'requestId': '',
+  'responseTime': 10,
+  'requestTime': 10,
+  'data': {
+    'key': '26c50ebac55b8a6db60dba8f641b5362',
+    'lng': 119.262117,
+    'lat': 26.051754,
+    'cityCode': '0591'
+  },
+  'responseContent': {
+    'advs': [
+      {
+        'href': 'http://qm.22ca.com/qm/tmpl/goods.html?goods_commonid=101654',
+        'imageUrl': 'http://qm.22ca.com/data/upload/mall/adv/05715959543890495.jpg'
+      },
+      {
+        'href': '',
+        'imageUrl': 'http://qm.22ca.com/data/upload/mall/adv/05730599933071664.png'
+      },
+      {
+        'href': '',
+        'imageUrl': 'http://qm.22ca.com/data/upload/mall/adv/05730600094085575.png'
+      }
+    ],
+    'recommendClasses': [
+      {
+        'gcName': '家居生活',
+        'gcId': '1036',
+        'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1036.jpg'
+      },
+      {
+        'gcName': '服装鞋帽',
+        'gcId': '1037',
+        'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1037.jpg'
+      },
+      {
+        'gcName': '母婴',
+        'gcId': '1058',
+        'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1058.jpg'
+      },
+      {
+        'gcName': '美食',
+        'gcId': '1073',
+        'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1073.jpg'
+      },
+      {
+        'gcName': '超市',
+        'gcId': '1091',
+        'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1091.jpg'
+      },
+      {
+        'gcName': '图书',
+        'gcId': '1064',
+        'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1064.jpg'
+      },
+      {
+        'gcName': '家用电器',
+        'gcId': '1074',
+        'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1074.jpg'
+      },
+      {
+        'gcName': '花',
+        'gcId': '1137',
+        'gcImage': 'http://qm.22ca.com/data/upload/mall/common/category-pic-1137.jpg'
+      }
+    ]
+  }
+}
