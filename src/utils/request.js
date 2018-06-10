ac4b671bcab5845d12c81ef72673605a6b3c2ec4
zhaojintian <729234283@qq.com>
2018-06-10 18:38:07 +0800

    update

--- src/utils/request.js
+++ src/utils/request.js
@@ -39,7 +39,7 @@ export default async (
     return new Promise(resolve => {
       setTimeout(() => {
         resolve(mockData.responseContent)
-      }, 200)
+      }, 100)
     })
   } else {
     const res = await wepy.request({
