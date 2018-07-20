3762a2d9b2b84d2e5dce8d77ada0131bbc8f32db
zhaojintian <729234283@qq.com>
2018-07-20 21:28:54 +0800

    优化

--- src/mixins/global.js
+++ src/mixins/global.js
@@ -38,6 +38,23 @@ export default class extends wepy.mixin {
       const { type, object, index = -1, childobject, childindex, key = 'image' } = e.currentTarget.dataset
       const url = `${consts.IMAGE_CDN_URL}/components/image/${type}.jpg`
 
+      const keys = e.currentTarget.dataset.keys
+
+      switch (keys.length) {
+        case 1:
+          this[keys[0]] = url
+          break
+        case 3:
+          this[keys[0]][keys[1]][keys[2]] = url
+          break
+        case 5:
+          this[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]] = url
+          break
+        default:
+          break
+      }
+      /*
+
       if (childobject) {
         this[object][index][childobject][childindex][key] = url
       } else if (key === '') {
@@ -47,6 +64,7 @@ export default class extends wepy.mixin {
       } else {
         this[object][index][key] = url
       }
+      */
     }
   }
 }
